8c4f7599588733e1c6fece05b089a08af7d30965
hbruckman <henry.bruckman@gmail.com>
2025-11-21 15:31:21 -0400

    Add: Delete Contact

--- src/ContactBook/ContactBook.cs
+++ src/ContactBook/ContactBook.cs
@@ -355,7 +355,7 @@ public class ContactBook
 
 		Console.WriteLine();
 		
-		if(Confirm("Do you want to update this contact?", YES))
+		if(Confirm("Do you want to update this contact?", NO))
 		{
 			c.SetFName(fname);
 			c.SetLName(lname);
@@ -371,7 +371,38 @@ public class ContactBook
 
 	private void DeleteContact()
 	{
+		int index = GetInt("Enter index", 1, allContacts.Count) - 1;
+
+		Console.Clear();
+
+		Console.WriteLine(new string('#', 80));
 		Console.WriteLine("Delete Contact");
+		Console.WriteLine(new string('#', 80));
+		Console.WriteLine();
+
+		DeleteContact(index);
+
+		Console.WriteLine();
+		PressEnterContinue();
+	}
+
+	private void DeleteContact(int index)
+	{
+		Contact c = allContacts[index];
+
+		ReviewContact(index);
+
+		Console.WriteLine();
+		
+		if(Confirm("Do you want to delete this contact?", NO))
+		{
+			allContacts.Remove(c);
+			Console.WriteLine("Operation successful: Contact deleted.");
+		}
+		else
+		{
+			Console.WriteLine("Operation cancelled: Contact not deleted.");
+		}
 	}
 
 	private void FindContacts()
