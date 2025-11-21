35802c773c2b7faddbfefc2312f7a97c0c5f0cf7
hbruckman <henry.bruckman@gmail.com>
2025-11-21 14:34:32 -0400

    Add: Create Contact

--- src/ContactBook/ContactBook.cs
+++ src/ContactBook/ContactBook.cs
@@ -239,7 +239,32 @@ public class ContactBook
 
 	private void CreateContact()
 	{
+		Console.Clear();
+		Console.WriteLine(new string('#', 80));
 		Console.WriteLine("Create Contact");
+		Console.WriteLine(new string('#', 80));
+		Console.WriteLine();
+
+		Console.Write("Enter first name: ");
+		string fname = Console.ReadLine()!;
+		Console.Write("Enter last name: ");
+		string lname = Console.ReadLine()!;
+		Console.Write("Enter phone: ");
+		string phone = Console.ReadLine()!;
+		Console.Write("Enter email: ");
+		string email = Console.ReadLine()!;
+		
+		if(Confirm("Do you want to create this contact?", YES))
+		{
+			Contact c = new Contact(fname, lname, phone, email);
+			allContacts.Add(c);
+			page = PageCount(allContacts, size);
+			Console.WriteLine("Operation successful: Contact created.");
+		}
+		else
+		{
+			Console.WriteLine("Operation cancelled: Contact not created.");
+		}
 	}
 
 	private void ReviewContact()
